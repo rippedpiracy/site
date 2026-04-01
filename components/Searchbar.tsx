87afc4c8b7a72030caff10861dde8fdc07f34fa9
rippedpiracy <rippedpiracy@tutanota.com>
2026-04-02 00:22:24 +0200

    fix many things

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -3,7 +3,7 @@ import { DocSearch } from "@docsearch/react";
 const config = {
   appId: import.meta.env.VITE_ALGOLIA_APP_ID ?? "YXKOI5NILL",
   apiKey: import.meta.env.VITE_ALGOLIA_API_KEY ?? "d23967e8d3cb288f2724946939799ede",
-  index: import.meta.env.VITE_ALGOLIA_INDEX_NAME ?? "ripped",
+  index: import.meta.env.VITE_ALGOLIA_INDEX_NAME ?? "ripped-guide",
 };
 
 export default function Searchbar() {
@@ -12,7 +12,7 @@ export default function Searchbar() {
       appId={config.appId}
       apiKey={config.apiKey}
       indices={[config.index]}
-      placeholder="Search documentation"
+      placeholder="Search docs"
       transformItems={(items) =>
         items.map((hit) => {
           const url = new URL(hit.url);
